8335f47799ded522b590d5160d6e8f3ba76c2e7e
Nightprince <keramatjokar8855@gmail.com>
2023-06-17 22:57:53 +0330

    Bug Fix: [cookie_helper.php] set_cookie was missing sameSite argument

--- system/helpers/cookie_helper.php
+++ system/helpers/cookie_helper.php
@@ -60,7 +60,7 @@ if ( ! function_exists('set_cookie'))
 	 *
 	 * @param	mixed
 	 * @param	string	the value of the cookie
-	 * @param	string	the number of seconds until expiration
+	 * @param	int	the number of seconds until expiration
 	 * @param	string	the cookie domain.  Usually:  .yourdomain.com
 	 * @param	string	the cookie path
 	 * @param	string	the cookie prefix
@@ -68,10 +68,10 @@ if ( ! function_exists('set_cookie'))
 	 * @param	bool	true makes the cookie accessible via http(s) only (no javascript)
 	 * @return	void
 	 */
-	function set_cookie($name, $value = '', $expire = '', $domain = '', $path = '/', $prefix = '', $secure = NULL, $httponly = NULL)
+	function set_cookie($name, $value = '', $expire = 0, $domain = '', $path = '/', $prefix = '', $secure = NULL, $httponly = NULL, $samesite = NULL)
 	{
 		// Set the config file options
-		get_instance()->input->set_cookie($name, $value, $expire, $domain, $path, $prefix, $secure, $httponly);
+		get_instance()->input->set_cookie($name, $value, $expire, $domain, $path, $prefix, $secure, $httponly, $samesite);
 	}
 }
 
