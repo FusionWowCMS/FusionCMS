1a092f853bc9552d1e0e809418831c2c5392d714
Nightprince <keramatjokar8855@gmail.com>
2023-08-06 15:46:55 +0330

    Fix Rbac system for Trinity 4.3.4

--- application/emulators/trinity_rbac_cata_soap.php
+++ application/emulators/trinity_rbac_cata_soap.php
@@ -4,7 +4,7 @@
  * Abstraction layer for supporting different emulators
  */
 
-class Trinity_cata_soap implements Emulator
+class Trinity_rbac_cata_soap implements Emulator
 {
     protected $config;
 
