d9aa9883ef1019a591ae8db04d172f6a7ea414a4
Nightprince <keramatjokar8855@gmail.com>
2023-11-15 12:54:46 +0330

    Rewrite Donate modules
    * Fix bug not save month income
    * Move codes from model to controller
    * clean

--- application/modules/donate/controllers/Donate.php
+++ application/modules/donate/controllers/Donate.php
@@ -1,5 +1,23 @@
 <?php
 
+require './vendor/autoload.php';
+
+//API Container
+use PayPal\Rest\ApiContext;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Api\PaymentExecution;
+
+//API Functions
+use PayPal\Api\Item;
+use PayPal\Api\Payer;
+use PayPal\Api\Amount;
+use PayPal\Api\Payment;
+use PayPal\Api\Details;
+use PayPal\Api\ItemList;
+use PayPal\Api\Transaction;
+use PayPal\Api\RedirectUrls;
+use PayPal\Exception\PayPalConnectionException;
+
 class Donate extends MX_Controller
 {
     public function __construct()
@@ -19,8 +37,6 @@ class Donate extends MX_Controller
 
         $this->template->setTitle(lang("donate_title", "donate"));
 
-        $donate_paypal = $this->config->item('donate_paypal');
-
         $user_id = $this->user->getId();
 
         $paypal = array(
@@ -31,7 +47,7 @@ class Donate extends MX_Controller
         {
             if ($this->input->post("donation_type") == "paypal")
             {
-                $this->paypal_model->getDonate($this->input->post("data_id"));
+                $this->getDonate($this->input->post("data_id"));
             }
         }
 
@@ -43,11 +59,11 @@ class Donate extends MX_Controller
             "currency_sign" => $this->config->item('donation_currency_sign'),
         );
 
-        $data['use_paypal'] = (!empty($this->config->item("paypal_userid")) && !empty($this->config->item("paypal_secretpass")) && $this->config->item("use_paypal")) ? true : false;
+        $data['use_paypal'] = !empty($this->config->item("paypal_userid")) && !empty($this->config->item("paypal_secretpass")) && $this->config->item("use_paypal");
 
         $output = $this->template->loadPage("donate.tpl", $data);
 
-        // Load the topsite page and format the page contents
+        // Load the top site page and format the page contents
         $pageData = array(
             "module" => "default",
             "headline" => breadcumb(array(
@@ -63,15 +79,69 @@ class Donate extends MX_Controller
         $this->template->view($page, "modules/donate/css/donate.css", "modules/donate/js/donate.js");
     }
 
-    public function canceled()
+    private function getApi()
     {
-        $this->paypal_model->setCanceled($this->input->get("token"), '2');
-        redirect(base_url('/donate'));
+        $api = new ApiContext(
+            new OAuthTokenCredential($this->config->item('paypal_userid'), $this->config->item('paypal_secretpass'))
+        );
+
+        $api->setConfig([
+            'mode' => $this->config->item('paypal_mode'),
+            'http.ConnectionTimeOut' => 30,
+            'log.LogEnabled' => false,
+            'log.FileName' => 'paypal_logs',
+            'log.LogLevel' => 'FINE',
+            'validation.level' => 'log'
+        ]);
+
+        return $api;
     }
 
     public function checkPaypal($id)
     {
-        $this->paypal_model->check($id);
+        $execute = new PaymentExecution();
+
+        $payment_id = $_GET['paymentId'];
+        $payerId = $_GET['PayerID'];
+        $payment = Payment::get($payment_id, $this->getApi());
+
+        $execute->setPayerId($payerId);
+        try {
+            $result = $payment->execute($execute, $this->getApi());
+
+            $payment_data = array(
+                'payer_email' => $result->payer->payer_info->email,
+                'invoice_number' => $result->transactions[0]->invoice_number,
+                'transactions_code' => $result->transactions[0]->related_resources[0]->sale->id,
+            );
+            $this->paypal_model->update_payment($payment_id, $payment_data);
+
+            $status = $this->paypal_model->getStatus($payment_id);
+
+            if ($status == '1') {
+                redirect(base_url('/donate'));
+            } else {
+                // transaction status
+                $this->paypal_model->setStatus($payment_id, "1");
+
+                $specify_donate = $this->paypal_model->getSpecifyDonate($id);
+
+                // update account
+                $this->donate_model->giveDp($this->user->getId(), $specify_donate['points']);
+
+                // update income
+                $this->donate_model->updateMonthlyIncome($specify_donate['price']);
+
+                redirect(base_url('/donate/success'));
+            }
+        } catch (Exception $e) {
+            $this->paypal_model->setStatus($payment_id, "3");
+            $this->paypal_model->setError($payment_id, $e);
+
+            log_message('error', $e);
+
+            redirect(base_url('/donate/error'));
+        }
     }
 
     public function success()
@@ -90,5 +160,118 @@ class Donate extends MX_Controller
         $page = $this->template->loadPage("error.tpl", $data);
 
         $this->template->box(lang("donate_error", "donate"), $page, true);
+    }
+
+    public function canceled()
+    {
+        $this->paypal_model->setCanceled($this->input->get("token"), '2');
+        redirect(base_url('/donate'));
+    }
+
+    private function getDonate($id)
+    {
+        $item = new Item();
+        $payer = new Payer();
+        $amount = new Amount();
+        $details = new Details();
+        $payment = new Payment();
+        $itemList = new ItemList();
+        $transaction = new Transaction();
+        $redirectUrls = new RedirectUrls();
+
+        $setTax = '0.00';
+        $setPrice = $this->paypal_model->getSpecifyDonate($id)['price'];
+        $setTotal = ($setTax + $setPrice);
+
+        //Payer
+        $payer->setPaymentMethod('paypal');
+
+        //item
+        $item->setName($this->paypal_model->getSpecifyDonate($id)['points'] . ' points')
+            ->setCurrency($this->config->item('donation_currency'))
+            ->setQuantity(1)
+            ->setPrice($setPrice);
+
+        //item list
+        $itemList->setItems([$item]);
+
+        //details
+        $details->setShipping('0.00')
+            ->setTax($setTax)
+            ->setSubtotal($setPrice);
+
+        $amount->setCurrency($this->config->item('donation_currency'))
+            ->setTotal($setTotal)
+            ->setDetails($details);
+
+        //transaction
+        $transaction->setAmount($amount)
+            ->setItemList($itemList)
+            ->setDescription('Purchase ' . $this->paypal_model->getSpecifyDonate($id)['points'] . ' points for user ' . $this->user->getId() . '')
+            ->setInvoiceNumber(uniqid());
+
+        //payment
+        $payment->setIntent('sale')
+            ->setPayer($payer)
+            ->setTransactions([$transaction]);
+
+        //redirect urls
+        $redirectUrls->setReturnUrl(base_url('/donate/checkPaypal/' . $id))
+            ->setCancelUrl(base_url('/donate/canceled'));
+
+        $payment->setIntent('sale')
+            ->setPayer($payer)
+            ->setRedirectUrls($redirectUrls)
+            ->setTransactions([$transaction]);
+
+        try {
+            $payment->create($this->getApi());
+
+            $hash = md5($payment->getId());
+
+            $date = new DateTime($payment->create_time);
+
+            $url_parts = parse_url($payment->links[1]->href,);
+
+            parse_str($url_parts['query'], $query_parts);
+
+            $token = $query_parts['token'];
+
+            //prepare and execute
+            $dataInsert = array(
+                'user_id' => $this->user->getId(),
+                'payment_id' => $payment->getId(),
+                'hash' => $hash,
+                'total' => $payment->transactions[0]->amount->total,
+                'points' => $this->paypal_model->getSpecifyDonate($id)['points'],
+                'create_time' => $date->getTimestamp(),
+                'currency' => $this->config->item('donation_currency'),
+                'error' => '',
+                'status' => '0',
+                'invoice_number' => '',
+                'payer_email' => '',
+                'token' => $token,
+            );
+
+            $this->db->insert('paypal_logs', $dataInsert);
+        } catch (PayPalConnectionException $e) {
+            log_message('error', $e);
+
+            if (preg_match('[500|501|502|503|504|60000]', $e)) {
+                $this->session->set_tempdata('paypal_error', 'PayPal is currently experiencing problems. Please try later', 10);
+                redirect(base_url('/donate/error'));
+            }
+            else
+            {
+                die($e);
+            }
+        }
+
+        foreach ($payment->getLinks() as $key => $link) {
+            if ($link->getRel() == 'approval_url') {
+                $redirectUrl = $link->getHref();
+            }
+        }
+        redirect($redirectUrl);
     }
 }
