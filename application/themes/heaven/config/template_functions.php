0f7a2ce22a76aec98b033db78c4fa2037f258acc
Darksider <43168059+code-path@users.noreply.github.com>
2023-11-23 10:50:59 +0330

    Update heaven theme
    Increase template_functions.php compatibility

--- application/themes/heaven/config/template_functions.php
+++ application/themes/heaven/config/template_functions.php
@@ -1,31 +1,34 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-# Register plugin to smarty template
-CI::$APP->smarty->registerPlugin('function', 'formatTitle', 'FN_formatTitle');
-
-/**
- * Smarty plugin formatTitle
- * @param  array  $params
- * @param  object $smarty
- * @return string
- */
-function FN_formatTitle($params, $smarty)
+if(!function_exists('FN_formatTitle'))
 {
-    // Initialize title
-    $title = '';
+    /**
+     * Smarty plugin formatTitle
+     * @param  array  $params
+     * @param  object $smarty
+     * @return string
+     */
+    function FN_formatTitle($params, $smarty)
+    {
+        // Initialize title
+        $title = '';
 
-    // Set title
-    if(isset($params['title']) && $params['title'])
-        $title = $params['title'];
+        // Set title
+        if(isset($params['title']) && $params['title'])
+            $title = $params['title'];
 
-    // Explode title
-    $title = explode(' ', $title);
+        // Explode title
+        $title = explode(' ', $title);
 
-    // Modify last item
-    if(count($title) > 1)
-        $title[count($title) - 1] = '<span>' . $title[count($title) - 1] . '</span>';
+        // Modify last item
+        if(count($title) > 1)
+            $title[count($title) - 1] = '<span>' . $title[count($title) - 1] . '</span>';
 
-    return implode(' ', $title);
+        return implode(' ', $title);
+    }
+
+    # Register plugin to smarty template
+    CI::$APP->smarty->registerPlugin('function', 'formatTitle', 'FN_formatTitle');
 }
 
 # Prevent errors
