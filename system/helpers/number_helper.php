ab818778bf72dc8a189aec978209c626588ce3d8
Nightprince <keramatjokar8855@gmail.com>
2023-12-18 00:15:33 +0330

    Update Number Helper

--- system/helpers/number_helper.php
+++ system/helpers/number_helper.php
@@ -1,41 +1,14 @@
 <?php
+
 /**
- * CodeIgniter
+ * This file is part of CodeIgniter 4 framework.
  *
- * An open source application development framework for PHP
+ * (c) CodeIgniter Foundation <admin@codeigniter.com>
  *
- * This content is released under the MIT License (MIT)
- *
- * Copyright (c) 2019 - 2022, CodeIgniter Foundation
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
- * @copyright	Copyright (c) 2019 - 2022, CodeIgniter Foundation (https://codeigniter.com/)
- * @license	https://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.0.0
- * @filesource
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
  */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
@@ -48,19 +21,59 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @link		https://codeigniter.com/userguide3/helpers/number_helper.html
  */
 
-// ------------------------------------------------------------------------
+if (! function_exists('number_to_roman')) {
+    /**
+     * Convert a number to a roman numeral.
+     *
+     * @param int|string $num it will convert to int
+     */
+    function number_to_roman($num): ?string
+    {
+        static $map = [
+            'M'  => 1000,
+            'CM' => 900,
+            'D'  => 500,
+            'CD' => 400,
+            'C'  => 100,
+            'XC' => 90,
+            'L'  => 50,
+            'XL' => 40,
+            'X'  => 10,
+            'IX' => 9,
+            'V'  => 5,
+            'IV' => 4,
+            'I'  => 1,
+        ];
+
+        $num = (int) $num;
+
+        if ($num < 1 || $num > 3999) {
+            return null;
+        }
+
+        $result = '';
+
+        foreach ($map as $roman => $arabic) {
+            $repeat = (int) floor($num / $arabic);
+            $result .= str_repeat($roman, $repeat);
+            $num %= $arabic;
+        }
+
+        return $result;
+    }
+}
 
 if ( ! function_exists('byte_format'))
 {
 	/**
 	 * Formats a numbers as bytes, based on size, and adds the appropriate suffix
 	 *
-	 * @param	mixed	will be cast as int
-	 * @param	int
-	 * @return	string
+	 * @param	mixed $num will be cast as int
+	 * @param	int $precision
+     * @return	string
 	 */
-	function byte_format($num, $precision = 1)
-	{
+	function byte_format($num, $precision = 1): string
+    {
 		$CI =& get_instance();
 		$CI->lang->load('number');
 
