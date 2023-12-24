48e6110958ea5c3cd70b6d7dcbebd03b7f27aaac
Nightprince <keramatjokar8855@gmail.com>
2023-12-24 11:34:51 +0330

    Update Benchmark

--- system/core/Benchmark.php
+++ system/core/Benchmark.php
@@ -26,17 +26,17 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- * @package	CodeIgniter
- * @author	EllisLab Dev Team
- * @copyright	Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
- * @copyright	Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
- * @copyright	Copyright (c) 2019 - 2022, CodeIgniter Foundation (https://codeigniter.com/)
- * @license	https://opensource.org/licenses/MIT	MIT License
- * @link	https://codeigniter.com
- * @since	Version 1.0.0
+ * @package    CodeIgniter
+ * @author     EllisLab Dev Team
+ * @copyright  Copyright (c) 2008 - 2014, EllisLab, Inc. (https://ellislab.com/)
+ * @copyright  Copyright (c) 2014 - 2019, British Columbia Institute of Technology (https://bcit.ca/)
+ * @copyright  Copyright (c) 2019 - 2022, CodeIgniter Foundation (https://codeigniter.com/)
+ * @license    https://opensource.org/licenses/MIT	MIT License
+ * @link       https://codeigniter.com
+ * @since      Version 1.0.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * Benchmark Class
@@ -44,91 +44,89 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * This class enables you to mark points and calculate the time difference
  * between them. Memory consumption can also be displayed.
  *
- * @package		CodeIgniter
- * @subpackage	Libraries
- * @category	Libraries
- * @author		EllisLab Dev Team
- * @link		https://codeigniter.com/userguide3/libraries/benchmark.html
+ * @package     CodeIgniter
+ * @subpackage  Libraries
+ * @category    Libraries
+ * @author      EllisLab Dev Team
+ * @link        https://codeigniter.com/userguide3/libraries/benchmark.html
  */
-class CI_Benchmark {
+class CI_Benchmark
+{
 
-	/**
-	 * List of all benchmark markers
-	 *
-	 * @var	array
-	 */
-	public $marker = array();
+    /**
+     * List of all benchmark markers
+     *
+     * @var    array
+     */
+    public array $marker = [];
 
-	/**
-	 * Set a benchmark marker
-	 *
-	 * Multiple calls to this function can be made so that several
-	 * execution points can be timed.
-	 *
-	 * @param	string	$name	Marker name
-	 * @return	void
-	 */
-	public function mark($name)
-	{
-		$this->marker[$name] = microtime(TRUE);
-	}
+    /**
+     * Set a benchmark marker
+     *
+     * Multiple calls to this function can be made so that several
+     * execution points can be timed.
+     *
+     * @param string $name Marker name
+     * @return    void
+     */
+    public function mark(string $name): void
+    {
+        $this->marker[$name] = microtime(true);
+    }
 
-	// --------------------------------------------------------------------
+    // --------------------------------------------------------------------
 
-	/**
-	 * Elapsed time
-	 *
-	 * Calculates the time difference between two marked points.
-	 *
-	 * If the first parameter is empty this function instead returns the
-	 * {elapsed_time} pseudo-variable. This permits the full system
-	 * execution time to be shown in a template. The output class will
-	 * swap the real value for this variable.
-	 *
-	 * @param	string	$point1		A particular marked point
-	 * @param	string	$point2		A particular marked point
-	 * @param	int	$decimals	Number of decimal places
-	 *
-	 * @return	string	Calculated elapsed time on success,
-	 *			an '{elapsed_string}' if $point1 is empty
-	 *			or an empty string if $point1 is not found.
-	 */
-	public function elapsed_time($point1 = '', $point2 = '', $decimals = 4)
-	{
-		if ($point1 === '')
-		{
-			return '{elapsed_time}';
-		}
+    /**
+     * Elapsed time
+     *
+     * Calculates the time difference between two marked points.
+     *
+     * If the first parameter is empty this function instead returns the
+     * {elapsed_time} pseudo-variable. This permits the full system
+     * execution time to be shown in a template. The output class will
+     * swap the real value for this variable.
+     *
+     * @param string $point1 A particular marked point
+     * @param string $point2 A particular marked point
+     * @param int $decimals  Number of decimal places
+     *
+     * @return    string    Calculated elapsed time on success,
+     *            an '{elapsed_string}' if $point1 is empty
+     *            or an empty string if $point1 is not found.
+     */
+    public function elapsed_time(string $point1 = '', string $point2 = '', int $decimals = 4): string
+    {
+        if ($point1 === '') {
+            return '{elapsed_time}';
+        }
 
-		if ( ! isset($this->marker[$point1]))
-		{
-			return '';
-		}
+        if (!isset($this->marker[$point1])) {
+            return '';
+        }
 
-		if ( ! isset($this->marker[$point2]))
-		{
-			$this->marker[$point2] = microtime(TRUE);
-		}
+        if (!isset($this->marker[$point2])) {
+            $this->marker[$point2] = microtime(TRUE);
+        }
 
-		return number_format($this->marker[$point2] - $this->marker[$point1], $decimals);
-	}
+        return number_format($this->marker[$point2] - $this->marker[$point1], $decimals);
+    }
 
-	// --------------------------------------------------------------------
+    // --------------------------------------------------------------------
 
-	/**
-	 * Memory Usage
-	 *
-	 * Simply returns the {memory_usage} marker.
-	 *
-	 * This permits it to be put it anywhere in a template
-	 * without the memory being calculated until the end.
-	 * The output class will swap the real value for this variable.
-	 *
-	 * @return	string	'{memory_usage}'
-	 */
-	public function memory_usage()
-	{
-		return '{memory_usage}';
-	}
+    /**
+     * Memory Usage
+     *
+     * Simply returns the {memory_usage} marker.
+     *
+     * This permits it to be put it anywhere in a template
+     * without the memory being calculated until the end.
+     * The output class will swap the real value for this variable.
+     *
+     * @return string '{memory_usage}'
+     */
+    public function memory_usage(): string
+    {
+        return '{memory_usage}';
+    }
 
 }
