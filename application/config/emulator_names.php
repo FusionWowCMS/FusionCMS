fcb0c79da2c1cf6eff93be5a1d31dc2c9931949c
Nightprince <keramatjokar8855@gmail.com>
2024-01-24 01:31:01 +0330

    Clean Emulator names

--- application/config/emulator_names.php
+++ application/config/emulator_names.php
@@ -1,20 +1,20 @@
 <?php
 
-$emulators = array(
-    "azerothcore"              => "AzerothCore (3.3.5a) - connected through SOAP",
-    "azerothcore_old"          => "AzerothCore (3.3.5a) Old Base - connected through SOAP",
-    "trinity"                  => "TrinityCore (3.3.5a) - connected through SOAP",
-    "trinity_df"               => "TrinityCore - Dragon Flight (10.x.x) - connected through SOAP",
-    "trinity_sl"               => "TrinityCore - Shadowlands (9.x.x) - connected through SOAP",
-    "trinity_legion"           => "TrinityCore - Legion (7.x.x) - connected through SOAP",
-    "trinity_cata"             => "TrinityCore - Cataclysm (4.3.4) - connected through SOAP",
-    "trinity_wotlkclassic"     => "TrinityCore - Wotlk Classic (3.4.3) - connected through SOAP",
-    "trinity_old"              => "TrinityCore (3.3.5a) Old Base - connected through SOAP",
-    "skyfire"                  => "Skyfire (5.4.8) - connected through SOAP",
-    "cmangos"                  => "CMangos (3.3.5a) - connected through SOAP",
-    "mangos_zero_sph"          => "Mangos Zero (1.12.1) - connected through SOAP",
-    "mangos_one_two_sph"       => "Mangos One/Two (2.4.3/3.3.5a) - connected through SOAP",
-    "mangos_three_sph"         => "Mangos Three (4.3.4) - connected through SOAP",
-    "vmangos"                  => "VMangos - Vanilla - connected through SOAP",
-    "oregoncore"               => "OregonCore (2.4.3) - connected through SOAP",
-);
+$emulators = [
+    'trinity'                  => 'TrinityCore (3.3.5a)',
+    'trinity_df'               => 'TrinityCore - Dragon Flight (10.x.x)',
+    'trinity_sl'               => 'TrinityCore - Shadowlands (9.x.x)',
+    'trinity_legion'           => 'TrinityCore - Legion (7.x.x)',
+    'trinity_cata'             => 'TrinityCore - Cataclysm (4.3.4)',
+    'trinity_wotlkclassic'     => 'TrinityCore - Wotlk Classic (3.4.3)',
+    'trinity_old'              => 'TrinityCore (3.3.5a) Old Base',
+    'skyfire'                  => 'Skyfire (5.4.8)',
+    'azerothcore'              => 'AzerothCore (3.3.5a)',
+    'azerothcore_old'          => 'AzerothCore (3.3.5a) Old Base',
+    'cmangos'                  => 'CMangos (3.3.5a)',
+    'mangos_zero_sph'          => 'Mangos Zero (1.12.1)',
+    'mangos_one_two_sph'       => 'Mangos One/Two (2.4.3/3.3.5a)',
+    'mangos_three_sph'         => 'Mangos Three (4.3.4)',
+    'vmangos'                  => 'VMangos - Vanilla',
+    'oregoncore'               => 'OregonCore (2.4.3)',
+];
