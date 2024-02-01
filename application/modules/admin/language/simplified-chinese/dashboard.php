7832ae8bf258a14e1561b1e7d749145d763cf14c
wolone <71954466@qq.com>
2024-02-02 00:40:58 +0800

    Add application\modules\admin\language\simplified-chinese

---
+++ application/modules/admin/language/simplified-chinese/dashboard.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang["important"] = "Important";
+$lang["new"] = "New";
+$lang["update_available"] = "A new update is available.";
+$lang["update_download"] = "You can update to new version from";
+$lang["here"] = "here";
+$lang["theme_old"] = "Your theme is outdated.";
+$lang["theme_old_detail"] = "Some pages may not display correctly for you, you should ask a frontend web or your frontend to update your theme.";
