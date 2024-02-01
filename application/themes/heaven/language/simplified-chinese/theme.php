0b12efd64ceeafd9e3c58687555a195a14a532b0
wolone <71954466@qq.com>
2024-02-02 00:44:07 +0800

    Add application\themes\heaven\language\simplified-chinese

---
+++ application/themes/heaven/language/simplified-chinese/theme.php
@@ -0,0 +1,55 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+# Lang array
+$lang = [
+    /**
+     * - RTL -----------------------------------------------------
+     * -----------------------------------------------------------
+     */
+    'isRTL' => 0,
+
+    /**
+     * - General -------------------------------------------------
+     * -----------------------------------------------------------
+     */
+    'general_or'         => 'or',
+    'general_icon'       => 'Icon',
+    'general_okay'       => '好的',
+    'general_accept'     => '接受',
+    'general_reject'     => 'Reject',
+    'general_cancel'     => '取消',
+    'general_online'     => '在线',
+    'general_offline'    => '离线',
+    'general_loading'    => '加载...',
+    'general_readMore'   => '查看更多',
+    'general_userAvatar' => '%s\'s 头像',
+
+    /**
+     * - Main Template -------------------------------------------
+     * -----------------------------------------------------------
+     */
+    'btn_logout'           => '退出',
+    'btn_register'         => '注册',
+    'btn_downloadLauncher' => 'Download launcher',
+
+    'wall_playNow'         => 'Play now',
+    'wall_userOnline'      => 'Browse %saccount panel%s or',
+    'wall_userOffline'     => 'Already a member? %slogin%s or',
+    'wall_playersOnline'   => '在线玩家',
+
+    'footer_copyright'     => '%s&copy;%s Copyright %s',
+
+    /**
+     * - Module: News --------------------------------------------
+     * -----------------------------------------------------------
+     */
+    'news_pageTitle' => '最新新闻',
+
+    /**
+     * - Module: Sidebox toppvp ----------------------------------
+     * -----------------------------------------------------------
+     */
+    'toppvp_rank'         => 'NR',
+    'toppvp_player'       => 'Player',
+    'toppvp_player_kills' => 'Player kills'
+];
