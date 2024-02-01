5af9c66658a2dedc64f38e28ca0f83c29319953b
wolone <71954466@qq.com>
2024-02-02 00:40:06 +0800

    Add application\language\simplified-chinese

---
+++ application/language/simplified-chinese/main.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * English language support
+ */
+
+/**
+ * 2-letter language abbreviation, used for flag icons
+ *
+ * @see http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+ */
+$lang['abbreviation'] = "cn";
+
+// User panel abbreviation
+$lang['ucp'] = "UCP";
+
+/**
+ * ====================================
+ * Time identifiers
+ * ====================================
+ */
+
+// Singular forms
+$lang['month'] = "month";
+$lang['day'] = "day";
+$lang['hour'] = "hour";
+$lang['minute'] = "minute";
+$lang['second'] = "second";
+
+// Plural forms
+$lang['months'] = "months";
+$lang['days'] = "days";
+$lang['hours'] = "hours";
+$lang['minutes'] = "minutes";
+$lang['seconds'] = "seconds";
+
+// Permission errors
+$lang['denied'] = "Permission denied";
+$lang['must_be_signed_in'] = "You must be signed in to view this page!";
+$lang['already_signed_in'] = "You are already signed in!";
+$lang['no_permission'] = "You do not have permission to view this page.";
+$lang['click_to_sign_in'] = "Please click here to sign in.";
+
+// Misc
+$lang['reason'] = "Reason:";
+$lang['banned'] = "banned";
+$lang['close_tool'] = "Close tool";
+$lang['offline'] = "离线";
+
+$lang['voting_points'] = "投票点";
+$lang['donation_points'] = "捐赠点";
+
+$lang['change_avatar'] = "更改头像";
+
+// UCP nav
+$lang['vote'] = "投票";
+$lang['donate'] = "捐赠";
+$lang['store'] = "商店";
+$lang['account_settings'] = "账户设置";
+$lang['account_security'] = "账户安全";
+$lang['teleport_hub'] = "Teleport Hub";
+
+$lang['gm_panel'] = "GM面板";
+$lang['admin_panel'] = "管理面板";
+
+$lang['login'] = "登录";
+$lang['logout'] = "退出";
+$lang['register'] = "注册";
+$lang['account'] = "账户";
+
