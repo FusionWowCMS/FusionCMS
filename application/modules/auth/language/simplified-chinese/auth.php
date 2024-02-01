7757943e9d9f3ba75ff624253726ff41fbab1a5a
wolone <71954466@qq.com>
2024-02-02 00:41:39 +0800

    Add application\modules\auth\language\simplified-chinese

---
+++ application/modules/auth/language/simplified-chinese/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+//Login
+$lang["login_label_user"] = "用户名";
+$lang["login_label_password"] = "密码";
+$lang["login_label_captcha"] = "验证码";
+$lang["login_label_remember"] = "记住我";
+$lang["login_link_password_forgot"] = "忘记密码?";
+$lang["login_button"] = "登录";
+$lang['log_in'] = "登录";
+
+//Errors
+$lang["error"] = "找不到具有给定凭据的帐户";
+$lang["ip_blocked"] = "您的IP已被阻止";
+$lang["try_again"] = "请再试一次";
+$lang["minutes"] = "分钟";
+
+//Captcha
+$lang["captcha_invalid"] = "验证码无效";
