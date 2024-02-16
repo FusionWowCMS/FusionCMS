6a9425fea55899e21c88e51727e2c41b2e295745
Nightprince <keramatjokar8855@gmail.com>
2024-02-16 19:26:03 +0330

    Fix search character armory for new changes Trinity

--- application/modules/armory/models/Armory_model.php
+++ application/modules/armory/models/Armory_model.php
@@ -20,7 +20,7 @@ class Armory_model extends CI_Model
         $searchString = $this->w_connection->escape_str($searchString);
 
         //Get the connection and run a query
-        $query = $this->w_connection->query("SELECT " . columns("item_template", array("entry", "name", "ItemLevel", "RequiredLevel", "InventoryType", "Quality", "class", "subclass"), $realmId) . " FROM " . table("item_template", $realmId) . " WHERE UPPER(" . column("item_template", "name", false, $realmId) . ") LIKE ? ORDER BY " . column("item_template", "ItemLevel", false, $realmId) . " DESC LIMIT ".$limit." OFFSET ".$offset, array('%' . strtoupper($searchString) . '%'));
+        $query = $this->w_connection->query("SELECT " . columns("item_template", ["entry", "name", "ItemLevel", "RequiredLevel", "InventoryType", "Quality", "class", "subclass"], $realmId) . " FROM " . table("item_template", $realmId) . " WHERE UPPER(" . column("item_template", "name", false, $realmId) . ") LIKE ? ORDER BY " . column("item_template", "ItemLevel", false, $realmId) . " DESC LIMIT ".$limit." OFFSET ".$offset, ['%' . strtoupper($searchString) . '%']);
 
         if ($query->num_rows() > 0) {
             return $query->result_array();
@@ -57,7 +57,7 @@ class Armory_model extends CI_Model
 
         $searchString = $this->c_connection->escape_str($searchString);
 
-        $query = $this->c_connection->query(query("find_guilds", $realmId), array('%' . $searchString . '%'));
+        $query = $this->c_connection->query(query("find_guilds", $realmId), ['%' . $searchString . '%']);
 
         if ($query->num_rows() > 0) {
             return $query->result_array();
@@ -88,10 +88,9 @@ class Armory_model extends CI_Model
 
         $searchString = $this->c_connection->escape_str($searchString);
 
-        //$query = "SELECT " . columns("characters", array("guid", "name", "race", "gender", "class", "level"), $realmId) . " FROM " . table("characters", $realmId) . " WHERE " . column("characters", "name", false, $realmId) . " LIKE '".$searchString."' LIMIT ".$limit." OFFSET ".$offset."";
-        $this->c_connection->select(columns("characters", array("guid", "name", "race", "gender", "class", "level"), $realmId));
+        $this->c_connection->select(columns("characters", ["guid", "name", "race", "gender", "class", "level"], $realmId));
         $this->c_connection->from(table("characters", $realmId));
-        $this->c_connection->like(column("characters", "name", false, $realmId), $searchString);
+        $this->c_connection->like(column("characters", "name", false, $realmId), ucfirst($searchString));
         $result = $this->c_connection->get();
 
         if ($result->num_rows() > 0) {
