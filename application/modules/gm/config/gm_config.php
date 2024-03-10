cb275928892983ffa09efcb7dde3754a9f93fd20
Nightprince <keramatjokar8855@gmail.com>
2024-03-10 22:28:21 +0330

    Module/GM: Fix send item from GM

--- application/modules/gm/config/gm_config.php
+++ application/modules/gm/config/gm_config.php
@@ -6,6 +6,10 @@ $config['gm_answertitle'] = "GM ticket answer";
 //Commands for ingame
 $config['gm_kickcommand'] = ".kick";
 
+//For the answering
+$config['gm_senditemtitle'] = "GM item delivery";
+$config['gm_senditembody'] = "A Game Master has sent you an item!";
+
 $config['gm_unstuck_position_x'] = "-1829.76";
 $config['gm_unstuck_position_y'] = "5362.1948";
 $config['gm_unstuck_position_z'] = "-12.427672";
