b2acef33f4681a3fab1dc5de5c38a1d053481d96
1825679767 <124947689+1825679767@users.noreply.github.com>
2024-09-13 10:26:18 +0800

    Add Simplified Chinese!
    Add Simplified Chinese!

---
+++ application/language/simplified-chinese/sidebox_shoutbox.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['ago'] = "前";
+$lang['view_profile'] = "查看个人资料";
+$lang['said'] = "说";
+$lang['message_limit'] = "消息长度必须在0-255个字符之间！";
+$lang['enter'] = "输入消息...";
+$lang['submit'] = "发送消息";
+$lang['log_in'] = "请登录后再发送";
---
+++ application/language/simplified-chinese/tooltip.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Note to module developers:
+ *  Keeping a module specific language file like this
+ *  in this external folder is not a good practise for
+ *  portability - I do not advice you to do this for
+ *  your own modules since they are non-default.
+ *  Instead, simply put your language files in
+ *  application/modules/yourModule/language/
+ *  You do not need to change any code, the system
+ *  will automatically look in that folder too.
+ */
+
+$lang['loading'] = "加载中...";
+$lang['no_item'] = "未指定物品或领域！";
+$lang['view_item'] = "查看物品";
+$lang['armory'] = "军械库";
+$lang['unknown_item'] = "未知物品";
+$lang['meta'] = "多彩插槽";
+$lang['red'] = "红色插槽";
+$lang['yellow'] = "黄色插槽";
+$lang['blue'] = "蓝色插槽";
+$lang['prismatic'] = "棱彩插槽";
+$lang['cogwheel'] = "齿轮插槽";
+$lang['domination'] = "统御插槽";
+$lang['hydraulic'] = "液压插槽";
+$lang['primordial'] = "原始插槽";
+$lang['tinker'] = "工匠插槽";
+$lang['relic-iron'] = "铁神器圣物插槽";
+$lang['relic-blood'] = "血神器圣物插槽";
+$lang['relic-shadow'] = "暗影神器圣物插槽";
+$lang['relic-fel'] = "邪能神器圣物插槽";
+$lang['relic-arcane'] = "奥术神器圣物插槽";
+$lang['relic-frost'] = "冰霜神器圣物插槽";
+$lang['relic-fire'] = "火焰神器圣物插槽";
+$lang['relic-water'] = "水神器圣物插槽";
+$lang['relic-life'] = "生命神器圣物插槽";
+$lang['relic-storm'] = "风暴神器圣物插槽";
+$lang['relic-holy'] = "神圣神器圣物插槽";
+$lang['red-punchcard'] = "红色打孔卡插槽";
+$lang['yellow-punchcard'] = "黄色打孔卡插槽";
+$lang['blue-punchcard'] = "蓝色打孔卡插槽";
+$lang['restores'] = "装备：恢复";
+$lang['increases'] = "装备：提高你的";
+$lang['by'] = "提高";
+$lang['armor'] = "护甲";
+$lang['damage'] = "伤害";
+$lang['speed'] = "速度";
+$lang['holy'] = "神圣抗性";
+$lang['nature'] = "自然抗性";
+$lang['fire'] = "火焰抗性";
+$lang['frost'] = "冰霜抗性";
+$lang['shadow'] = "暗影抗性";
+$lang['arcane'] = "奥术抗性";
+$lang['durability'] = "耐久度";
+$lang['requires_level'] = "需要等级";
+$lang['item_level'] = "物品等级";
+$lang['unknown_effect'] = "未知效果";
+$lang['dps'] = "每秒伤害";
