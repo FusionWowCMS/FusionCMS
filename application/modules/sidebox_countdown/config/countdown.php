f1ec63490c82572c3aaafe8da21d8786ce9872e3
Nightprince <keramatjokar8855@gmail.com>
2024-09-25 17:42:38 +0330

    Implement Sidebox Countdown Timer

---
+++ application/modules/sidebox_countdown/config/countdown.php
@@ -0,0 +1,5 @@
+<?php
+
+$config['Date_Timestamp'] = 1730312400;
+
+$config['text'] = 'Winter Is Coming!';
---
+++ application/modules/sidebox_countdown/controllers/Countdown.php
@@ -0,0 +1,43 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+use MX\MX_Controller;
+
+/**
+ * Countdown Controller Class
+ */
+class Countdown extends MX_Controller
+{
+    const string DS = DIRECTORY_SEPARATOR;
+
+    private static string $moduleUrl;
+    private static string $moduleName;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->config('sidebox_countdown/countdown');
+
+        // Get module name
+        self::$moduleName = basename(str_replace('controllers', '', __DIR__));
+
+        // Get module url
+        self::$moduleUrl = rtrim(base_url(), '/') . '/' . basename(APPPATH) . '/modules/' . self::$moduleName . '/';
+    }
+
+    /**
+     * View
+     * @return string
+     */
+    public function view(): string
+    {
+        $data = [
+            'module'       => self::$moduleName,
+            'moduleUrl'    => self::$moduleUrl,
+            'date'         => $this->config->item('Date_Timestamp'),
+            'text'         => $this->config->item('text'),
+        ];
+
+        return $this->template->loadPage('countdown.tpl', $data);
+    }
+}
