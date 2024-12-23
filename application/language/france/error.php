dfd4f9a105357c03a11fdf55462a4b0b75b6d897
Flying Phoenix <andrei081993@gmail.com>
2024-12-24 00:24:42 +0100

    (ADD) Missing France parts

---
+++ application/language/france/error.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Note to module developers:
+ *  Keeping a module specific language file like this
+ *  in this external folder is not a good practise for
+ *  portability - I do not advice you to do this for
+ *  your own modules since they are non-default.
+ *  Instead, simply put your language files in
+ *  application/modules/yourModule/language/
+ *  You do not need to change any code, the system
+ *  will automatically look in that folder too.
+ */
+
+ $lang['404_title'] = "Fichier introuvable";
+ $lang['404'] = "Erreur 404 - fichier introuvable";
+ $lang['404_long'] = "La page que vous cherchez n'existe pas. Comment vous êtes arrivé ici reste un mystère. Mais vous pouvez cliquer sur le bouton ci-dessous pour revenir à la page d'accueil.";
+ $lang['permission_denied_title'] = "Accès refusé";
+ $lang['permission_denied'] = "Vous n'avez pas la permission d'accéder à cette page !";
+ $lang['lost'] = "OH NON ! Vous êtes perdu.";
+ $lang['button_home'] = "Accueil";
+ 
---
+++ application/language/france/wow_tooltip.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * Only translate these if World of Warcraft does it themselves,
+ * otherwise you'll confuse people who expect to see them in English
+ * Don't touch the null-entries, they are just there to avoid errors
+ */
+
+ $lang['speed'] = "Vitesse";
+
+ $lang["bind"] = array(
+     0 => null,
+     1 => "Se lie lorsqu'on ramassé",
+     2 => "Se lie lorsqu'équipé",
+     3 => "Se lie lorsqu'utilisé",
+     4 => "Objet de quête"
+ );
+ 
+ $lang["slots"] = array(
+     0 => null,
+     1 => "Tête",
+     2 => "Cou",
+     3 => "Épaules",
+     4 => "Chemise",
+     5 => "Torse",
+     6 => "Taille",
+     7 => "Jambes",
+     8 => "Pieds",
+     9 => "Poignets",
+     10 => "Mains",
+     11 => "Doigt",
+     12 => "Bijou",
+     13 => "Une main",
+     14 => "Bouclier",
+     15 => "Distance",
+     16 => "Dos",
+     17 => "Deux mains",
+     18 => "Sac",
+     19 => "Tabard",
+     20 => "Robe",
+     21 => "Main principale",
+     22 => "Main secondaire",
+     23 => "Maintenable",
+     24 => "Munitions",
+     25 => "Lancé",
+     26 => "Distance",
+     27 => "Carquois",
+     28 => "Relique"
+ );
+ 
+ $lang["damages"] = array(
+     0 => null, // Physique
+     1 => "Sacré",
+     2 => "Feu",
+     3 => "Nature",
+     4 => "Frost",
+     5 => "Ombre",
+     6 => "Arcane"
+ );
+ 
+ $lang["spelltriggers"] = array(
+     0 => "Utiliser: ",
+     1 => "Équiper: ",
+     2 => "Chance lors d'un coup: ",
+     3 => "Inconnu: ",
+     4 => "Inconnu: ",
+     5 => "Inconnu: ",
+     6 => "Inconnu: "
+ );
+ 
+ $lang["armor_sub"] = array(
+     0 => "Divers",
+     1 =>  "Tissu",
+     2 => "Cuir",
+     3 => "Maille",
+     4 => "Plaque",
+     5 => null,
+     6 => "Bouclier",
+     7 => "Libram",
+     8 => "Idole",
+     9 =>  "Totem",
+     10 => "Sigil",
+ );
+ 
+ $lang["weapon_sub"] = array(
+     0 => "Hache",
+     1 => "Hache",
+     2 => "Arc",
+     3 => "Fusil",
+     4 => "Masse",
+     5 => "Masse",
+     6 => "Arme d'hast",
+     7 => "Épée",
+     8 => "Épée",
+     9 => "Obsolète",
+     10 => "Bâton",
+     11 => "Exotique",
+     12 => "Exotique",
+     13 => "Arme de poing",
+     14 => "Divers",
+     15 => "Dague",
+     16 => "Lancé",
+     17 => "Lance",
+     18 => "Arbalète",
+     19 => "Bâton magique",
+     20 => "Canne à pêche"
+ );
+ 
+ $lang["stats"] = array(
+     0 => "Mana",
+     1 => "Santé",
+     3 => "Agilité",
+     4 => "Force",
+     5 => "Intelligence",
+     6 => "Esprit",
+     7 => "Endurance",
+     12 => "Évaluation de défense",
+     13 => "Évaluation d'esquive",
+     14 => "Évaluation de parade",
+     15 => "Évaluation de blocage",
+     16 => "Évaluation de coup au corps à corps",
+     17 => "Évaluation de coup à distance",
+     18 => "Évaluation de coup de sort",
+     19 => "Évaluation de critique au corps à corps",
+     20 => "Évaluation de critique à distance",
+     21 => "Évaluation de critique de sort",
+     28 => "Évaluation de hâte au corps à corps",
+     29 => "Évaluation de hâte à distance",
+     30 => "Évaluation de hâte de sort",
+     31 => "Évaluation de coup",
+     32 => "Évaluation de critique",
+     35 => "Évaluation de résilience",
+     36 => "Évaluation de hâte",
+     37 => "Évaluation d'expertise",
+     38 => "Puissance d'attaque",
+     39 => "Puissance d'attaque à distance",
+     40 => "Puissance d'attaque féroce",
+     41 => "Soin",
+     42 => "Dégâts avec les sorts",
+     43 => "Mana par 5 sec.",
+     44 => "Évaluation de pénétration d'armure",
+     45 => "Puissance des sorts",
+     46 => "Santé par 5 sec.",
+     47 => "Évaluation de pénétration de sort",
+     48 => "Valeur de blocage"
+ );
+ 
