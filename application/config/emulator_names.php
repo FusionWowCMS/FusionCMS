2be608e494aa0712d3e2a89b532b133698e1eeec
Nightprince <keramatjokar8855@gmail.com>
2025-06-21 22:14:05 +0330

    App/Emulators: Fix mangos emulators name. Close #125

--- application/config/emulator_names.php
+++ application/config/emulator_names.php
@@ -15,9 +15,9 @@ $emulators = [
     'azerothcore'              => 'AzerothCore (3.3.5a)',
     'azerothcore_old'          => 'AzerothCore (3.3.5a) Old Base',
     'cmangos'                  => 'CMangos (3.3.5a)',
-    'mangos_zero_sph'          => 'Mangos Zero (1.12.1)',
-    'mangos_one_two_sph'       => 'Mangos One/Two (2.4.3/3.3.5a)',
-    'mangos_three_sph'         => 'Mangos Three (4.3.4)',
+    'mangos_zero'              => 'Mangos Zero (1.12.1)',
+    'mangos_one_two'           => 'Mangos One/Two (2.4.3/3.3.5a)',
+    'mangos_three'             => 'Mangos Three (4.3.4)',
     'vmangos'                  => 'VMangos - Vanilla',
     'oregoncore'               => 'OregonCore (2.4.3)',
 ];
